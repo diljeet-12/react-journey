acc60c6ce5a8106fcff18e8a959743542f6b712f
diljeet-12 <bhatiwal2005@gmail.com>
2026-09-02 20:55:25 +0530

    Learning API Fetching with useEffect

---
+++ 11-React Revision practice/Day 9 - API Fetching with useEffect/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
